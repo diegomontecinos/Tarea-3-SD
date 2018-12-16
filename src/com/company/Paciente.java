5f607b6223d1e746a000d862228681ff6022c0c8
diegomontecinos <diegomontecinos@users.noreply.github.com>
2018-12-16 17:50:34 -0300

    probando commit intellij

--- src/com/company/Paciente.java
+++ src/com/company/Paciente.java
@@ -11,12 +11,14 @@ public class Paciente {
     ArrayList<Tratamiento> lista_tratamientos = new ArrayList<Tratamiento>();
     ArrayList<Examen> lista_examenes = new ArrayList<Examen>();
     ArrayList<Medicamento> lista_medicamentos = new ArrayList<Medicamento>();
-    Paciente(String name, int age, String r, ArrayList<Enfermedad> l_enf, ArrayList<Tratamiento> l_trat, ArrayList<Examen> exam, ArrayList<Medicamento> med){
+    Paciente(String name, int age, String r, ArrayList<Enfermedad> l_enf, ArrayList<Tratamiento> l_trat, ArrayList<Examen> l_exam, ArrayList<Medicamento> l_med){
 
         this.nombre = name;
         this.edad = age;
         this.rut = rut;
-        this.lista_enfermedades
-
+        this.lista_enfermedades = l_enf;
+        this.lista_tratamientos = l_trat;
+        this.lista_examenes = l_exam;
+        this.lista_medicamentos = l_med;
     }
 }
