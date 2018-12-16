efad80e2008ad7e6de72b5ecd86b1728cef14044
diegomontecinos <diegomontecinos@users.noreply.github.com>
2018-12-16 18:00:54 -0300

    Clases definidas

--- src/com/company/Requerimientos.java
+++ src/com/company/Requerimientos.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Requerimientos {
+
+    int tipo;
+    int id_tipo;
+
+    ArrayList<Paciente> lista_pacientes = new ArrayList<Paciente>();
+
+    Requerimientos(int t, int id, ArrayList<Paciente> l_p){
+        this.tipo = t;
+        this.id_tipo = id;
+        this.lista_pacientes = l_p;
+    }
+
+
 }
